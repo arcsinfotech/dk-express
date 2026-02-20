ed898221774e0484a65ff1bb4af720dd206a2301
Robin <robin@arcsinfotech.com>
2026-02-20 19:17:27 +0530

    Add carousel component with autoplay functionality, create button and utility functions, and introduce components.json for configuration

--- src/components/HeroSlideShow.tsx
+++ src/components/HeroSlideShow.tsx
@@ -1,7 +1,32 @@
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
+
 export default function HeroSlideShow() {
+  const autoplay = useRef(
+    Autoplay({
+      delay: 5000, // 2 seconds
+      stopOnInteraction: false, // keeps autoplay after manual drag
+    })
+  );
+
   return (
     <div className="w-[820px] p-3 h-[605px] border border-[#FFFFFF47] rounded-[26px] backdrop-blur-[60px]">
-        <img src="/src/assets/images/truck1.png" alt="Hero Slide Show" className="w-full h-full object-cover rounded-[16px]" />
+        <Carousel className="w-full h-full" opts={{ loop: true, watchDrag: false }} plugins={[autoplay.current]}>
+          <CarouselContent className="h-full!">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <CarouselItem key={index} className="h-full basis-full flex items-stretch">
+                <div className="w-full h-full">
+                    <img
+                      src={`/src/assets/images/truck${index + 1}.png`}
+                      alt="Hero Slide Show"
+                      className="w-full h-full object-cover rounded-[16px]"
+                    />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
     </div>
   )
 }
