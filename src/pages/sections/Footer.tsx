ad2f259dd482218d29a896bb9c5c9775c3081601
Robin <robin@arcsinfotech.com>
2026-02-23 15:49:56 +0530

    feat: Update footer contact information and replace favicon.

--- src/pages/sections/Footer.tsx
+++ src/pages/sections/Footer.tsx
@@ -199,7 +199,7 @@ export default function Footer() {
                       clipRule="evenodd"
                     />
                   </svg>
-                  200 West Center Way, Stockton, CA
+                  2000 West Charter Way, Stockton, CA 95206
                 </li>
                 <li className="flex items-center gap-3 text-[18px] text-[#989898] font-medium whitespace-nowrap leading-[24px]">
                   <svg
@@ -220,7 +220,7 @@ export default function Footer() {
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  Dispatch@Dkexpress.Net
+                  dispatch@dkexpress.net
                 </li>
                 <li className="flex items-center gap-3 text-[18px] text-[#989898] font-medium whitespace-nowrap leading-[24px]">
                   <svg
