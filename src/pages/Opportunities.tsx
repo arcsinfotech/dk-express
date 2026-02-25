cb2ccb4c32b24a9c7c48dc1d13d1e6e222cc5442
Robin <robin@arcsinfotech.com>
2026-02-25 20:05:28 +0530

    style: improve card styling with blur effects and layout adjustments in Opportunities and apply formatting changes in Services.

--- src/pages/Opportunities.tsx
+++ src/pages/Opportunities.tsx
@@ -14,7 +14,6 @@ import foc from "@/assets/foc.svg";
 import Quote from "./sections/Quote";
 
 const Opportunities = () => {
-
   const cards = [
     {
       icon: <img src={ModernFleet} alt="Modern Fleet" width={32} height={32} />,
@@ -22,53 +21,118 @@ const Opportunities = () => {
       description: "All equipment under 4 years old with latest technology.",
     },
     {
-      icon: <img src={AssetCarrier} alt="Asset-Based Carrier" width={32} height={32} />,
+      icon: (
+        <img
+          src={AssetCarrier}
+          alt="Asset-Based Carrier"
+          width={32}
+          height={32}
+        />
+      ),
       title: "Asset-Based Carrier",
       description: "We own our rucks - no middlemen, direct service.",
     },
     {
-      icon: <img src={OwnerOperator} alt="Owner-Operator" width={32} height={32} />,
+      icon: (
+        <img src={OwnerOperator} alt="Owner-Operator" width={32} height={32} />
+      ),
       title: "Owner-Operators Welcome",
       description: "Competitive compensation and consistent freight.",
     },
     {
-      icon: <img src={Growth} alt="Growth Opportunities" width={32} height={32} />,
+      icon: (
+        <img src={Growth} alt="Growth Opportunities" width={32} height={32} />
+      ),
       title: "Growth Opportunities",
       description: "Expanding operations with room for advancement.",
     },
-  ]
+  ];
 
-  function getCard({icon, title, description}: {icon: React.ReactNode, title: string, description: string}) {
+  function getCard({
+    icon,
+    title,
+    description,
+  }: {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+  }) {
     return (
-      <div key={title} className="flex flex-col gap-4 p-[26px]">
-        <div className="w-[58px] h-[58px] bg-[#E64949] rounded-full flex items-center justify-center">
-          {icon}
+      <div key={title} className="flex flex-col p-[26px] gap-4 rounded-[26px] relative overflow-hidden">
+        <div className="absolute bg-[#E64949] rounded-full w-[186px] h-[186px] top-[231px] left-[-300px] z-0 blur-[150px]" />
+        <div className="absolute bg-[#E64949] rounded-full w-[186px] h-[186px] top-[31px] left-[329px] z-0 blur-[150px]" />
+        <div className="absolute bg-[#E64949] rounded-full w-[186px] h-[186px] top-[140px] left-[-118px] z-0 blur-[150px]" />
+        <div className="flex flex-col gap-4">
+          <div className="w-[58px] h-[58px] bg-[#E64949] rounded-full flex items-center justify-center">
+            {icon}
+          </div>
+          <div
+            className="text-[24px] text-[#262626] font-semibold capitalize"
+            style={{ fontFamily: "'Raleway', sans-serif" }}
+          >
+            {title}
+          </div>
         </div>
-        <div className="text-[24px] text-[#262626] font-semibold capitalize" style={{ fontFamily: "'Raleway', sans-serif" }}>{title}</div>
-        <div className="text-[16px] text-[#545454] mt-[14px] capitalize" style={{ fontFamily: "'Figtree', sans-serif" }}>{description}</div>
+        <div className="h-px bg-[#D2D2D2]" />
+        <div
+          className="text-[16px] text-[#545454] capitalize"
+          style={{ fontFamily: "'Figtree', sans-serif" }}
+        >
+          {description}
+        </div>
       </div>
-    )
+    );
   }
 
   const reasons = [
     {
-      icon: <img src={cpb} alt="Competitive pay & benefits" width={32} height={32} />,
-      title:"Competitive pay & benefits",
+      icon: (
+        <img
+          src={cpb}
+          alt="Competitive pay & benefits"
+          width={32}
+          height={32}
+        />
+      ),
+      title: "Competitive pay & benefits",
     },
     {
-      icon: <img src={cyrf} alt="Consistent, year-round freight" width={32} height={32} />,
+      icon: (
+        <img
+          src={cyrf}
+          alt="Consistent, year-round freight"
+          width={32}
+          height={32}
+        />
+      ),
       title: "Consistent, year-round freight",
     },
     {
-      icon: <img src={mef} alt="Modern equipment fleet" width={32} height={32} />,
+      icon: (
+        <img src={mef} alt="Modern equipment fleet" width={32} height={32} />
+      ),
       title: "Modern equipment fleet",
     },
     {
-      icon: <img src={CoolVan} alt="Dedicated dispatch support" width={32} height={32} />,
+      icon: (
+        <img
+          src={CoolVan}
+          alt="Dedicated dispatch support"
+          width={32}
+          height={32}
+        />
+      ),
       title: "Dedicated dispatch support",
     },
     {
-      icon: <img src={scr} alt="Strong customer relationships" width={32} height={32} />,
+      icon: (
+        <img
+          src={scr}
+          alt="Strong customer relationships"
+          width={32}
+          height={32}
+        />
+      ),
       title: "Strong customer relationships",
     },
     {
@@ -77,64 +141,86 @@ const Opportunities = () => {
     },
   ];
 
-
-  function getReason({icon, title}: {icon: React.ReactNode, title: string}) {
+  function getReason({
+    icon,
+    title,
+  }: {
+    icon: React.ReactNode;
+    title: string;
+  }) {
     return (
-      <div key={title} className="flex w-[310px] bg-[#FFFFFF1A] rounded-[16px] h-fit items-center gap-4 p-3">
+      <div
+        key={title}
+        className="flex w-[310px] bg-[#FFFFFF1A] rounded-[16px] h-fit items-center gap-4 p-3"
+      >
         <div className="w-[75px] h-[75px] rounded-[12px] bg-[#FFFFFF1A] flex items-center justify-center">
           {icon}
         </div>
-        <div className="text-[18px] leading-[22px] font-normal capitalize text-white" style={{ fontFamily: "'Figtree', sans-serif" }}>
+        <div
+          className="text-[18px] leading-[22px] font-normal capitalize text-white"
+          style={{ fontFamily: "'Figtree', sans-serif" }}
+        >
           {title}
         </div>
       </div>
-    )
+    );
   }
-
 
   return (
     <div className="min-h-screen">
-
       <div className="px-[135px] py-[120px]">
         <div className="flex flex-col items-center justify-center gap-[26px] w-full">
-          <div className="text-[54px] text-[#262626] font-semibold" style={{ fontFamily: "'Raleway', sans-serif" }}>
+          <div
+            className="text-[54px] text-[#262626] font-semibold"
+            style={{ fontFamily: "'Raleway', sans-serif" }}
+          >
             Your Success <span className="text-[#E64949]">Starts Here</span>
           </div>
-          <div className="text-[18px] text-[#545454] text-center px-100 capitalize font-normal" style={{ fontFamily: "'Figtree', sans-serif" }}>
-            Experience reliability, growth, and innovation with DK Express. From our modern fleet to direct service operations, we're committed to supporting drivers and partners with opportunity, stability, and cutting-edge logistics solutions.
+          <div
+            className="text-[18px] text-[#545454] text-center px-100 capitalize font-normal"
+            style={{ fontFamily: "'Figtree', sans-serif" }}
+          >
+            Experience reliability, growth, and innovation with DK Express. From
+            our modern fleet to direct service operations, we're committed to
+            supporting drivers and partners with opportunity, stability, and
+            cutting-edge logistics solutions.
           </div>
 
           <div className="mt-[34px] flex gap-4 items-center justify-center">
-            {cards.map((card) => (
-              getCard(card)
-            ))}
+            {cards.map((card) => getCard(card))}
           </div>
         </div>
       </div>
 
       <div className="bg-[#02112E] px-[135px] py-[120px] flex items-center justify-between">
-          <div className="max-w-[496px]">
-            <div className="text-[54px] leading-[64px] text-[#FFFFFF] font-semibold" style={{ fontFamily: "'Raleway', sans-serif" }}>
-              Why Choose <br /><span className="text-[#E64949]">DK Express</span>
-            </div>
-            <div className="mt-3 text-[18px] text-white font-normal capitalize" style={{ fontFamily: "'Figtree', sans-serif" }}>
-              At DK Express, we value our drivers and partners. We offer steady freight, great pay, and a supportive culture that drives success every mile.
-            </div>
+        <div className="max-w-[496px]">
+          <div
+            className="text-[54px] leading-[64px] text-[#FFFFFF] font-semibold"
+            style={{ fontFamily: "'Raleway', sans-serif" }}
+          >
+            Why Choose <br />
+            <span className="text-[#E64949]">DK Express</span>
           </div>
+          <div
+            className="mt-3 text-[18px] text-white font-normal capitalize"
+            style={{ fontFamily: "'Figtree', sans-serif" }}
+          >
+            At DK Express, we value our drivers and partners. We offer steady
+            freight, great pay, and a supportive culture that drives success
+            every mile.
+          </div>
+        </div>
 
-          <div className="grid grid-cols-3 gap-4">
-            {reasons.map((reason) => (
-              getReason(reason)
-            ))}
-          </div>
+        <div className="grid grid-cols-3 gap-4">
+          {reasons.map((reason) => getReason(reason))}
+        </div>
       </div>
 
-
-      <Quote 
-        title="Ready To" 
-        title2="Get Started?" 
-        description="Contact us today to learn more about partnership opportunities." 
-        buttonText="Become a Partner" 
+      <Quote
+        title="Ready To"
+        title2="Get Started?"
+        description="Contact us today to learn more about partnership opportunities."
+        buttonText="Become a Partner"
         path="/contact"
       />
       <Footer />
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,4 +1,3 @@
-
 import DryVan from "@/assets/dry_van.svg";
 import CoolVan from "@/assets/cool_van.svg";
 import FastVan from "@/assets/fast_van.svg";
@@ -10,7 +9,9 @@ import Footer from "./sections/Footer";
 
 const services = [
   {
-    icon: <img src={CoolVan} alt="Refrigerated Transport" width={32} height={32} />,
+    icon: (
+      <img src={CoolVan} alt="Refrigerated Transport" width={32} height={32} />
+    ),
     title: "Refrigerated Transport",
     details: [
       "53' food-grade reefers",
@@ -34,7 +35,9 @@ const services = [
     ],
   },
   {
-    icon: <img src={FastVan} alt="Storage & Re-Delivery" width={32} height={32} />,
+    icon: (
+      <img src={FastVan} alt="Storage & Re-Delivery" width={32} height={32} />
+    ),
     title: "Storage & Re-Delivery",
     details: [
       "Stockton, CA warehouse facility",
@@ -71,23 +74,76 @@ const services = [
   },
 ];
 
-function ServiceCard({ icon, title, details }: { icon: React.ReactNode, title: string, details: string[] }) {
+function ServiceCard({
+  icon,
+  title,
+  details,
+}: {
+  icon: React.ReactNode;
+  title: string;
+  details: string[];
+}) {
   return (
-    <div className="p-[26px]">
-      <div className="w-[58px] h-[58px] bg-[#E64949] rounded-full flex items-center justify-center mb-4">
-        {icon}
-      </div>
-      
-      <div className="text-[24px] font-semibold capitalize mb-[40px]" style={{ fontFamily: "Raleway" }}>
-        {title}
-      </div>
+    <div className="p-[26px] rounded-[26px] bg-white relative overflow-hidden">
+      <div className="absolute top-[229px] left-[-117px] w-[186px] h-[186px] bg-[#E64949] rounded-full blur-[200px]" />
+      <div className="absolute top-[-147px] left-[473px] w-[173px] h-[173px] bg-[#E64949] rounded-full blur-[200px]" />
+      <div className="flex justify-between items-center">
+        <div>
+          <div className="w-[58px] h-[58px] bg-[#E64949] rounded-full flex items-center justify-center mb-4">
+            {icon}
+          </div>
 
-      {/* Bullet Points */}
+          <div
+            className="text-[24px] font-semibold capitalize mb-[14px]"
+            style={{ fontFamily: "Raleway" }}
+          >
+            {title}
+          </div>
+        </div>
+        <svg
+          width="100"
+          height="100"
+          viewBox="0 0 100 100"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_377_3220)">
+            <path
+              d="M64.9062 36.0312L76.9987 48.1237C77.6568 48.6995 78.5781 50.1967 76.9987 51.5787C75.4193 52.9607 68.279 60.0516 64.9062 63.4244"
+              stroke="#E0DDDD"
+              stroke-linecap="round"
+            />
+            <rect
+              x="49.5414"
+              y="0.707103"
+              width="69.2937"
+              height="69.2937"
+              rx="5.42284"
+              transform="rotate(44.8109 49.5414 0.707103)"
+              stroke="#E0DDDD"
+            />
+            <path
+              d="M32.5781 50.3438H65.8941"
+              stroke="#E0DDDD"
+              stroke-linecap="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_377_3220">
+              <rect width="99.4097" height="99.4097" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+      <div className="h-px bg-[#E0DDDD] mb-[26px]" />
       <div className="grid grid-cols-2 gap-x-10 gap-y-4">
         {details.map((item, index) => (
           <div key={index} className="flex items-start gap-2">
             <span className="mt-[6px] w-[6px] h-[6px] bg-[#545454] rounded-full shrink-0"></span>
-            <span className="text-[16px] text-[#545454]" style={{ fontFamily: "Figtree" }}>
+            <span
+              className="text-[16px] text-[#545454]"
+              style={{ fontFamily: "Figtree" }}
+            >
               {item}
             </span>
           </div>
@@ -98,31 +154,42 @@ function ServiceCard({ icon, title, details }: { icon: React.ReactNode, title: s
 }
 
 const Services = () => {
-
   return (
     <div className="min-h-screen">
-      
       {/* services */}
       <div className="w-full h-full flex items-start gap-4 px-[135px]">
-        
         <div className="sticky top-[10%]">
-          <div className="relative overflow-hidden min-w-[678px] h-[536px] rounded-[26px] px-[25px] py-[30px] my-[120px]" style={{ background: "linear-gradient(169.27deg, #E64949 30.45%, #681F1F 92.04%)"}}>
+          <div
+            className="relative overflow-hidden min-w-[678px] h-[536px] rounded-[26px] px-[25px] py-[30px] my-[120px]"
+            style={{
+              background:
+                "linear-gradient(169.27deg, #E64949 30.45%, #681F1F 92.04%)",
+            }}
+          >
             <div className="absolute top-[280px] p-[102px] left-[220px] w-[627px] h-[627px] rounded-full bg-[#C73232] blur-2xl">
               <div className="w-[423px] h-[423px] bg-[#AE2222] rounded-full blur-[20px]"></div>
             </div>
-            <div className="font-semibold text-[54px] leading-[58px] text-white mb-[16px]" style={{ fontFamily: "Raleway" }}>
+            <div
+              className="font-semibold text-[54px] leading-[58px] text-white mb-[16px]"
+              style={{ fontFamily: "Raleway" }}
+            >
               Reliable Freight & Logistics Services
             </div>
-            <div className="text-[16px] capitalize text-white font-normal" style={{ fontFamily: "Figtree" }}>
-              We deliver reliable, efficient logistics from refrigerated transport to cross-docking, ensuring your freight arrives safely and on time.
+            <div
+              className="text-[16px] capitalize text-white font-normal"
+              style={{ fontFamily: "Figtree" }}
+            >
+              We deliver reliable, efficient logistics from refrigerated
+              transport to cross-docking, ensuring your freight arrives safely
+              and on time.
             </div>
           </div>
         </div>
 
         <div className="no-scrollbar flex flex-col gap-4 h-full w-full py-[120px]">
-        {services.map((service, index) => (
-          <ServiceCard key={index} {...service} />
-        ))}
+          {services.map((service, index) => (
+            <ServiceCard key={index} {...service} />
+          ))}
         </div>
       </div>
 
