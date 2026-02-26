945b801668985875b7b3c832ecb809199cb3c4a9
Robin <robin@arcsinfotech.com>
2026-02-26 20:32:00 +0530

    Worked on responsiveness of about-us page

--- src/components/CommonSubHeader.tsx
+++ src/components/CommonSubHeader.tsx
@@ -19,12 +19,15 @@ const CommonSubHeader = ({
       <div className="bg-[#00112D] bg-[radial-gradient(164.75%_164.13%_at_90.7%_-30.3%,#E64949_15.28%,#00112D_65.5%)]  flex flex-col gap-4 justify-center items-center h-full py-[209px]">
         <div className="flex flex-col gap-3 justify-center items-center ">
           <div
-            className="bg-[#FFFFFF14] text-white px-4 py-2 rounded-[36px] text-lg leading-[22px] tracking-normal"
+            className="bg-[#FFFFFF14] text-white px-4 py-2 rounded-[36px] max-[768px]:text-sm text-lg leading-[22px] tracking-normal"
             style={{ fontFamily: "'Outfit', sans-serif" }}
           >
             {heading}
           </div>
-          <div className={`${breakLine ? "flex flex-col gap-0" : "flex gap-4"} text-[92px] leading-[102px] tracking-normal font-medium`} style={{ fontFamily: "'Raleway', sans-serif" }}>
+          <div
+            className={`${breakLine ? "flex flex-col gap-0" : "flex gap-4"} text-[92px] max-[768px]:text-[48px] max-[768px]:leading-[52px] leading-[102px] tracking-normal font-medium`}
+            style={{ fontFamily: "'Raleway', sans-serif" }}
+          >
             <span className="text-white">{subHeading}</span>
             <span className="text-[#E64949] font-black">
               {secondSubHeading}
@@ -32,7 +35,7 @@ const CommonSubHeader = ({
           </div>
         </div>
         <div
-          className="text-lg leading-[22px] tracking-normal text-white max-w-[505px] text-center capitalize"
+          className="text-lg max-[768px]:text-sm leading-[22px] max-[768px]:leading-[20px] tracking-normal text-white max-w-[505px] text-center capitalize"
           style={{ fontFamily: "'Figtree', sans-serif" }}
         >
           {description}
