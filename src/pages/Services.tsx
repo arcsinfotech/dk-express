be84527f84c8985bbef2c65928a7e2b45c6f42f6
Robin <robin@arcsinfotech.com>
2026-03-03 19:56:35 +0530

    style: improve Services page responsiveness by adding and adjusting styles for 1280px and 768px breakpoints.

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -19,26 +19,25 @@ function ServiceCard({
 }) {
   return (
     <div className="p-[26px] max-[768px]:p-[16px] rounded-[26px] bg-white relative overflow-hidden">
-      
-      <div className="absolute top-[229px] left-[-117px] w-[186px] h-[186px] bg-[#E64949] rounded-full blur-[200px]" />
-      <div className="absolute top-[-147px] left-[473px] w-[173px] h-[173px] bg-[#E64949] rounded-full blur-[200px]" />
+      <div className="absolute top-[229px] left-[-117px] max-[1280px]:top-[122px] max-[1280px]:left-[-110px] w-[186px] h-[186px] bg-[#E64949] rounded-full blur-[200px]" />
+      <div className="absolute top-[-147px] left-[473px] max-[1280px]:top-0 max-[1280px]:left-[1000px] max-[768px]:left-[500px] w-[173px] h-[173px] bg-[#E64949] rounded-full blur-[200px]" />
 
       <div className="flex justify-between items-center">
         <div>
-          <div className="w-[58px] h-[58px] max-[768px]:w-[48px] max-[768px]:h-[48px] bg-[#E64949] rounded-full flex items-center justify-center mb-4 max-[768px]:mb-2">
+          <div className="w-[58px] h-[58px] max-[1280px]:w-[48px] max-[1280px]:h-[48px] bg-[#E64949] rounded-full flex items-center justify-center mb-4 max-[1280px]:mb-2">
             {icon}
           </div>
 
           <div
-            className="text-[24px] max-[768px]:text-[18px] font-semibold capitalize mb-[14px]"
+            className="text-[24px] max-[768px]:text-[18px] max-[1280px]:text-[20px] font-semibold capitalize mb-[14px] max-[1280px]:mb-[16px]"
             style={{ fontFamily: "Raleway" }}
           >
             {title}
           </div>
         </div>
         <svg
-          width="100"
-          height="100"
+          width={window.innerWidth > 1280 ? "100" : "60"}
+          height={window.innerWidth > 1280 ? "100" : "60"}
           viewBox="0 0 100 100"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -71,8 +70,8 @@ function ServiceCard({
           </defs>
         </svg>
       </div>
-      <div className="h-px bg-[#E0DDDD] mb-[26px]" />
-      <div className="grid grid-cols-2 max-[768px]:grid-cols-1 gap-x-10 gap-y-4 max-[768px]:gap-y-2">
+      <div className="h-px bg-[#E0DDDD] mb-[26px] max-[1280px]:mb-[16px]" />
+      <div className="grid grid-cols-2 max-[768px]:grid-cols-1 gap-x-10 max-[1280px]:gap-x-8 gap-y-4 max-[1280px]:gap-y-2 max-[768px]:gap-y-2">
         {details.map((item, index) => (
           <div key={index} className="flex items-start gap-2">
             <span className="mt-[6px] max-[768px]:mt-[8px] w-[6px] h-[6px] bg-[#545454] rounded-full shrink-0"></span>
@@ -90,13 +89,17 @@ function ServiceCard({
 }
 
 const Services = () => {
+  const isMobile = useIsMobile();
 
-  const isMobile = useIsMobile()
-  
   const services = [
     {
       icon: (
-        <img src={CoolVan} alt="Refrigerated Transport" width={ isMobile ? "24" : "32"} height={ isMobile ? "24" : "32"} />
+        <img
+          src={CoolVan}
+          alt="Refrigerated Transport"
+          width={isMobile ? "24" : "32"}
+          height={isMobile ? "24" : "32"}
+        />
       ),
       title: "Refrigerated Transport",
       details: [
@@ -109,7 +112,14 @@ const Services = () => {
       ],
     },
     {
-      icon: <img src={DryVan} alt="Dry Van" width={ isMobile ? "24" : "32"} height={ isMobile ? "24" : "32"} />,
+      icon: (
+        <img
+          src={DryVan}
+          alt="Dry Van"
+          width={isMobile ? "24" : "32"}
+          height={isMobile ? "24" : "32"}
+        />
+      ),
       title: "Dry Van",
       details: [
         "53' standard dry vans",
@@ -122,7 +132,12 @@ const Services = () => {
     },
     {
       icon: (
-        <img src={FastVan} alt="Storage & Re-Delivery" width={ isMobile ? "24" : "32"} height={ isMobile ? "24" : "32"} />
+        <img
+          src={FastVan}
+          alt="Storage & Re-Delivery"
+          width={isMobile ? "24" : "32"}
+          height={isMobile ? "24" : "32"}
+        />
       ),
       title: "Storage & Re-Delivery",
       details: [
@@ -135,7 +150,14 @@ const Services = () => {
       ],
     },
     {
-      icon: <img src={Team} alt="Team Driving" width={ isMobile ? "24" : "32"} height={ isMobile ? "24" : "32"} />,
+      icon: (
+        <img
+          src={Team}
+          alt="Team Driving"
+          width={isMobile ? "24" : "32"}
+          height={isMobile ? "24" : "32"}
+        />
+      ),
       title: "Team Driving",
       details: [
         "Non-stop delivery for time-sensitive freight",
@@ -147,7 +169,14 @@ const Services = () => {
       ],
     },
     {
-      icon: <img src={CrossDock} alt="Cross Dock" width={ isMobile ? "24" : "32"} height={ isMobile ? "24" : "32"} />,
+      icon: (
+        <img
+          src={CrossDock}
+          alt="Cross Dock"
+          width={isMobile ? "24" : "32"}
+          height={isMobile ? "24" : "32"}
+        />
+      ),
       title: "Cross Dock",
       details: [
         "Single-source logistics solution",
@@ -163,10 +192,10 @@ const Services = () => {
   return (
     <div className="min-h-screen">
       {/* services */}
-      <div className="w-full h-full flex max-[768px]:flex-col items-start gap-4 px-[135px] max-[768px]:px-[20px]">
-        <div className="sticky top-[10%] max-[768px]:static">
+      <div className="w-full h-full flex max-[1280px]:flex-col items-start gap-4 px-[135px] max-[768px]:px-[20px] max-[1280px]:px-[30px] max-[1280px]:pb-[80px] max-[768px]:pb-[60px] bg-[#F0F0F0]">
+        <div className="sticky top-[10%] max-[1280px]:static">
           <div
-            className="relative overflow-hidden min-w-[678px] max-[768px]:min-w-full h-[536px] max-[768px]:h-[328px] rounded-[26px] px-[25px] py-[30px] my-[120px] max-[768px]:my-0 max-[768px]:mt-[76px]"
+            className="relative overflow-hidden min-w-[678px] max-[1280px]:min-w-full h-[536px] max-[1280px]:h-[328px] rounded-[26px] px-[25px] py-[30px] my-[120px] max-[1280px]:my-0 max-[768px]:mt-[76px] max-[1280px]:mt-[80px]"
             style={{
               background:
                 "linear-gradient(169.27deg, #E64949 30.45%, #681F1F 92.04%)",
@@ -176,13 +205,13 @@ const Services = () => {
               <div className="w-[423px] h-[423px] bg-[#AE2222] rounded-full blur-[20px]"></div>
             </div>
             <div
-              className="font-semibold text-[54px] max-[768px]:text-[32px] leading-[58px] max-[768px]:leading-[38px] text-white mb-[16px]"
+              className="font-semibold text-[54px] max-[768px]:text-[32px] max-[1280px]:text-[48px] leading-[58px] max-[768px]:leading-[38px] max-[1280px]:leading-[54px] text-white mb-[16px]"
               style={{ fontFamily: "Raleway" }}
             >
-              Reliable Freight & Logistics Services
+              Reliable Freight & <br /> Logistics Services
             </div>
             <div
-              className="text-[16px] max-[768px]:text-[14px] max-[768px]:leading-[20px] capitalize text-white font-normal"
+              className="text-[16px] max-[768px]:text-[14px] max-[768px]:leading-[20px] capitalize text-white font-normal max-[1280px]:w-1/2 max-[768px]:w-full"
               style={{ fontFamily: "Figtree" }}
             >
               We deliver reliable, efficient logistics from refrigerated
@@ -192,7 +221,7 @@ const Services = () => {
           </div>
         </div>
 
-        <div className="no-scrollbar flex flex-col gap-4 h-full w-full py-[120px] max-[768px]:py-0">
+        <div className="no-scrollbar flex flex-col gap-4 max-[1280px]:gap-3 h-full w-full py-[120px] max-[1280px]:py-0">
           {services.map((service, index) => (
             <ServiceCard key={index} {...service} />
           ))}
